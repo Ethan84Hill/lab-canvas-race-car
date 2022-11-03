fdffc218161775172e4589cf3ac7377e527990b7
Ethan <ethan84hill@gmail.com>
2022-11-03 15:24:14 -0400

    updated movement and added random obstacles

--- js/index.js
+++ js/index.js
@@ -2,46 +2,78 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-  
-  function startGame() {
-    console.log('hello')
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-
-    const imgRoad = new Image();
-    imgRoad.src = 'images/road.png';
-     // imgRoad.onload = function() {
-    ctx.drawImage(imgRoad, 0, 0, 500, 700)
-    // }
-
-
-      let xCord = 225
-      let yCord = 600
-       const imgCar = new Image();
-       imgCar.src = 'images/car.png'
-      ctx.drawImage(imgCar, xCord, yCord, 50, 90)
-      window.addEventListener('keydown', function(event) {
-        // const imgCar = new Image();
-        // imgCar.src = 'images/ca4r.png'
-        ctx.clearRect(0, 0, 500, 700)
-        ctx.drawImage(imgRoad, 0, 0, 500, 700)
-        ctx.drawImage(imgCar, xCord, yCord, 50, 90)
-        switch(event.code) {
-          case 'ArrowRight':
-          xCord += 10;
-           break;
-          case 'ArrowLeft':
-            xCord -= 10;
-            break;
-        }
-      })
-    
-     
-
-
-    
-  }
 };
+  console.log('hello')
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  let dodge;
+
+
+
+  function startGame() {
+      driveloop()
+  }
+
+
+  const keys = [];
+  window.addEventListener("keydown", function (e) {
+      keys[e.which] = true;
+    });
+    window.addEventListener("keyup", function (e) {
+      keys[e.which] = false;
+    });
+
+  function driveloop() {
+    dodge = window.requestAnimationFrame(driveloop, canvas);
+    ctx.clearRect(0, 0, 500, 700)
+
+    if (keys[39]) {raceCar.xCord += 5;}
+    if (keys[37]) {raceCar.xCord -= 5;}
+
+    road.draw();
+    raceCar.draw();
+    obstacles.draw();
+  }
+
+
+  let xCord = 225
+  let yCord = 600
+  const imgCar = new Image();
+  imgCar.src = 'images/car.png'
+  let raceCar = {
+    xCord: 225,
+    yCord: 600,
+    width: 50,
+    height: 90,
+    draw: function() {
+      ctx.save();
+      ctx.drawImage(imgCar, this.xCord, this.yCord, this.width, this.height)
+      ctx.restore();
+    }
+  }
+
+  let obstacles = {
+    w: Math.floor(Math.random() * canvas.width),
+    h: 30,
+    speed: 1, 
+    x: Math.floor(Math.random() * canvas.width),
+    y: 0,
+    color: 'red',
+    draw: function() {  
+      ctx.fillStyle = 'brown'
+      ctx.fillRect(obstacles.x, obstacles.y, obstacles.w, obstacles.h)
+    }
+  }
+
+
+  const imgRoad = new Image();
+  imgRoad.src = 'images/road.png';
+
+  let road = {
+    draw: function() {
+      ctx.drawImage(imgRoad, 0, 0, 500, 700)
+    }
+  }
 
  
 
@@ -49,68 +81,20 @@ window.onload = () => {
 
 
 
- class CarImage{
-   constructor(){
-     this.x = 225;
-     this.y = 600;
-     this.width = width;
-     this.height = height;
-     this.imgCar = imgCar;
-     this.draw();
-     // let xCord = 225
-     // let yCord = 600
-      const imgCar = new Image();
-     imgCar.src = 'images/car.png'
-      //ctx.drawImage(imgCar, xCord, yCord, 50, 90)
-   }
+ 
 
-  
-   moveLeft(){
-     this.x -= 10;
-   }
-   moveRight(){
-     this.x += 10;
-   }
-   draw() {
-     ctx.drawImage(this.imgCar, this.x, this.y, 50, 90)
-   }
-
-   collisionCheck(obstacle){
-     if (
-       this.x < obstacle.x + obstacle.width &&
-       this.x + this.width > obstacle.x &&
-       this.y < obstacle.y + obstacle.height &&
-       this.height + this.y > obstacle.y
-     ) {
-       // Collision detected!
-       return true;
+  //  collisionCheck(obstacle) {
+  //    if (
+  //      this.x < obstacle.x + obstacle.width &&
+  //      this.x + this.width > obstacle.x &&
+  //      this.y < obstacle.y + obstacle.height &&
+  //      this.height + this.y > obstacle.y
+  //    ) {
+  //      // Collision detected!
+  //      return true;
       
-     } else {
-       // No collision
-       return false;
-     }
-   }
-
- }
-
- const raceCar = new CarImage();
-
- window.addEventListener('keydown', function(event) {
-   switch(event.code) {
-     case 'ArrowRight':
-       raceCar.moveRight();
-       break;
-     case 'ArrowLeft':
-       raceCar.moveLeft();
-       break;
-   }
-   updateCanvas();
- })
-
- function updateCanvas() {
-   ctx.clearRect(0, 0, 500, 700);
-
-   raceCar.draw()
- }
-
- updateCanvas()
+  //    } else {
+  //      // No collision
+  //      return false;
+  //    }
+  //  }
